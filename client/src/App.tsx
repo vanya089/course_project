87a37bd717455e1bb8122e67b91c8ab32dae99d2
vanya <vanchello089@gmail.com>
2023-08-22 15:05:50 +0600

    start 2

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline bg-red-500">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
