2468c81878a40788efa139fc7b00cb00f0e7b32e
vanya <vanchello089@gmail.com>
2023-08-31 18:08:37 +0600

    deploy vercel

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api", router)
-app.use(ApiErrorHandler);
+//app.use(ApiErrorHandler);
 
 async function startApp() {
     try {
