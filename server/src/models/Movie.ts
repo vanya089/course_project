fba004b9f901b850b678f76abc23223082a81f11
vanya <vanchello089@gmail.com>
2023-08-31 16:14:07 +0600

    add new func for server

--- server/src/models/Movie.ts
+++ server/src/models/Movie.ts
@@ -1,1 +1,15 @@
-console.log()
+import { Schema, model, Document, Model } from "mongoose";
+
+interface IMovie extends Document {
+    title: string;
+    description: string;
+    movieImageUrl: string;
+}
+
+const Movie: Model<IMovie> = model<IMovie>('Movie', new Schema<IMovie>({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    movieImageUrl: { type: String, required: true },
+}));
+
+export default Movie;
