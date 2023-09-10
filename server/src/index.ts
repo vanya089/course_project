b47c76b28faa833e67e455fb414126d6c084f7b5
vanya <vanchello089@gmail.com>
2023-09-10 16:00:07 +0600

    add google and twitter auth

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,10 +11,7 @@ const DB_URL = process.env.DB_URL;
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
+app.use(cors());
 app.use("/api", router)
 //app.use(ApiErrorHandler);
 
